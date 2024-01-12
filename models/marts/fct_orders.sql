0743a87e4a7c5259fc1a6d02a68009ec893e90b9
Anna Beatriz Vercesi <vercesiam@gmail.com>
2024-01-12 10:42:22 +0000

    Modified fact table and stg_orders files

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -17,6 +17,10 @@ with
     select
         customers.sk_customers
         , orders.*
+        , order_details.product_id
+        , order_details.unit_price
+        , order_details.quantity
+        , order_details.discount
     from orders
     left join customers on orders.customer_id = customers.customer_id
     left join order_details on orders.order_id = order_details.order_id
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -2,6 +2,10 @@ with
     orders as (
         select *
         from {{source('northwind', 'orders')}}
+        {% if env_var("DBT_TARGET_SCHEMA") == 'dev' %}
+    -- this filter will only be applied to the development schema
+    limit 100
+        {% endif %}
     )
 
 select *
